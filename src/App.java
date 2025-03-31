a3a3f1f28f9f841af67da73d61729c6d901c929b
Jason Wolf <jaywolfy@users.noreply.github.com>
2025-03-31 09:28:13 +0200

    Update Jason Wolf

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Jason!");
+        System.out.println("Hello, Jason Wolf!");
     }
 }
